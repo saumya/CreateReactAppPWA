395cae88889cbbbcaf3f283f324ff106732664d9
saumya <2saumyaray@gmail.com>
2017-05-20 22:54:39 +0530

    added react-tap-event-plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap |  http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
