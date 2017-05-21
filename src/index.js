4ed23f1d931448fefa37d4573f730d726cd21892
saumya <2saumyaray@gmail.com>
2017-05-21 12:13:48 +0530

    first rendeing

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap |  http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+//injectTapEventPlugin();
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+//import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+// enabling tap event
+injectTapEventPlugin();
+//rendering the application
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//registerServiceWorker();
