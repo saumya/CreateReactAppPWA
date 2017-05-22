67bf64e4886a2b773544185ddc383abed1d0a5d8
saumya <2saumyaray@gmail.com>
2017-05-22 21:26:27 +0530

    wip, basic running of application

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        {/*
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,6 +14,8 @@ class App extends Component {
         <p className="App-intro">
           React 15.5.4 with a default PWA from create-react-app script
         </p>
+        */}
+        <div> The app will render here. </div>
       </div>
     );
   }
